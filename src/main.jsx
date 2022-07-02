42fc4b3a8e681b15a6df27eb2f873255ac9cdf5d
GOJAx64 <josealbertogarcia453@gmail.com>
2022-07-01 22:03:21 -0500

    useContext

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import { CounterApp } from './01-useState/CounterApp';
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { SimpleForm } from './02-useEffect/SimpleForm';*/
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { TodoApp } from './08-useReducer/TodoApp';
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+//import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp/>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <MainApp />
+    </React.StrictMode>
+  </BrowserRouter>
 )
